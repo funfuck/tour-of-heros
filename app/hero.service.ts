dfe42dc5aca4190cef06338388202f55d7ed664a
funfuck <gglfrbeckham@gmail.com>
2016-10-30 12:59:28 +0700

    Delete a hero

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -45,5 +45,13 @@ export class HeroService {
       .catch(this.handleError);
   }
 
+  delete(id: number): Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
+
 }
 
