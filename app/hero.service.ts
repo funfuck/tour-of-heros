3a517d383ed411f26dfb25527817ca5dd49b6e63
funfuck <gglfrbeckham@gmail.com>
2016-10-30 12:26:57 +0700

    Update hero details

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -8,6 +8,7 @@ import { Hero } from './hero';
 @Injectable()
 export class HeroService {
   private heroesUrl = 'app/heroes';  // URL to web api
+  private headers = new Headers({'Content-Type': 'application/json'});
 
   constructor(private http: Http) { } 
 
@@ -23,6 +24,14 @@ export class HeroService {
                .then(heroes => heroes.find(hero => hero.id === id));
   }
 
+  update(hero: Hero): Promise<Hero> {
+    const url = `${this.heroesUrl}/${hero.id}`;
+    return this.http
+      .put(url, JSON.stringify(hero), {headers: this.headers})
+      .toPromise()
+      .then(() => hero)
+      .catch(this.handleError);
+  }
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error); // for demo purposes only
     return Promise.reject(error.message || error);
